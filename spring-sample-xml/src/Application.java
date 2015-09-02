0ef20ca8f70da34162d127a965879b1179e3a60d
ssandri-vpa <ssandri@velocitypartners.net>
2015-09-02 16:58:09 -0300

    Migrated to Eclipse

--- spring-sample-xml/src/Application.java
+++ spring-sample-xml/src/Application.java
@@ -1,5 +1,7 @@
 import ar.com.azioth.service.CustomerService;
 import ar.com.azioth.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by Sebastian Sandri on 27/08/2015.
@@ -7,8 +9,13 @@ import ar.com.azioth.service.CustomerServiceImpl;
 public class Application {
 
     public static void main(String[] args) {
-        CustomerService service = new CustomerServiceImpl();
+        //CustomerService service = new CustomerServiceImpl();
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-sample-xml/applicationContext.xml");
+
+        CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
 
         System.out.print(service.findAll().get(0).getFirstName());
+
     }
 }
--- spring-sample-xml/src/ar/com/azioth/service/CustomerServiceImpl.java
+++ spring-sample-xml/src/ar/com/azioth/service/CustomerServiceImpl.java
@@ -11,7 +11,14 @@ import java.util.List;
  */
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+
+    private CustomerRepository customerRepository;
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+
 
     @Override
     public List<Customer> findAll() {
